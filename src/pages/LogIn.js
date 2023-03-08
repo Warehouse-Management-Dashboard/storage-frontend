ca47bec70f67ed3b1dbc733509a77213da3342da
erwin <erwin1704code@gmail.com>
2023-03-08 09:39:53 +0700

    state management in login

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Stack } from "react-bootstrap";
 import "../assets/stylesheet/login.css";
 const LogIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState(true);
+
+  console.log(email, password, rememberMe);
   return (
     <div className="full-container position-relative">
       <Stack
@@ -11,20 +16,39 @@ const LogIn = () => {
         <h1 className="h4 text-center">Sign In</h1>
         <form className="vstack gap-3">
           <div className="form-input-container">
-            <input type="text" className="form-input" placeholder=" " />
+            <input
+              type="text"
+              className="form-input"
+              placeholder=" "
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
             <label className="form-label-input">Email</label>
           </div>
 
           <div className="form-input-container">
-            <input type="password" className="form-input" placeholder=" " />
+            <input
+              type="password"
+              className="form-input"
+              placeholder=" "
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
             <label className="form-label-input">Password</label>
           </div>
           <div className="form-checkbox-container ">
-            <input type="checkbox" id="remember-me" />
+            <input
+              type="checkbox"
+              id="remember-me"
+              onChange={(e) => setRememberMe(!rememberMe)}
+              checked={rememberMe}
+            />
             <label className="form-checkbox" for="remember-me"></label>
             <label for="remember-me">Remember Me?</label>
           </div>
-          <Button className="button-primary-edit">Sign In</Button>
+          <Button className="button-primary-edit" type="submit">
+            Sign In
+          </Button>
         </form>
       </Stack>
     </div>
